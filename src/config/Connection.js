573fbd4b83555c8dc546235601d9f4f90e9d7548
medjamiaanes <medjamia.anes@gmail.com>
2020-12-26 10:42:58 +0100

    Replaced sequelize with mongoose in the database service and started using mongoDB

--- src/config/Connection.js
+++ src/config/Connection.js
@@ -1,25 +1,19 @@
 const development = {
   database: process.env.DEVELOP_DB_NAME,
-  username: process.env.DEVELOP_DB_USER,
-  password: process.env.DEVELOP_DB_PASS,
   host: process.env.DEVELOP_DB_HOST,
-  dialect: 'postgres',
+  port: process.env.DEVELOP_DB_PORT || 27017,
 }
 
 const staging = {
   database: process.env.STAGING_DB_NAME,
-  username: process.env.STAGING_DB_USER,
-  password: process.env.STAGING_DB_PASS,
   host: process.env.STAGING_DB_HOST,
-  dialect: 'postgres',
+  port: process.env.STAGING_DB_PORT || 27017,
 }
 
 const production = {
   database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
   host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
+  port: process.env.PRODUCTION_DB_PORT || 27017,
 }
 
 module.exports = {
