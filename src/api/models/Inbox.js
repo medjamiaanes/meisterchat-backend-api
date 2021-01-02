cee0736326a35a9de8f1629a0f18703bf9e15d03
medjamiaanes <medjamia.anes@gmail.com>
2021-01-02 21:21:23 +0100

    Updated the Inbox model schema

--- src/api/models/Inbox.js
+++ src/api/models/Inbox.js
@@ -1,34 +1,39 @@
 const { Schema, model } = require('mongoose')
 
 const InboxSchema = Schema({
-  userId: {
+  user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
   },
-  messages: [
+  chats: [
     {
-      senderId: {
+      user: {
         type: Schema.Types.ObjectId,
         ref: 'User',
       },
-      verficationId: {
-        type: Schema.Types.String,
-      },
-      type: {
-        type: Schema.Types.String,
-        enum: ['image', 'video', 'file', 'text'],
-        default: 'text',
-      },
-      value: {
-        type: Schema.Types.String,
-      },
-      read: {
-        type: Schema.Types.Boolean,
-        default: false,
-      },
-      timestamp: {
-        type: Schema.Types.Date,
-      },
+      messages: [
+        {
+          type: {
+            type: Schema.Types.String,
+            enum: ['image', 'video', 'file', 'text'],
+            default: 'text',
+          },
+          textMessage: {
+            type: Schema.Types.String,
+          },
+          read: {
+            type: Schema.Types.Boolean,
+            default: false,
+          },
+          isSender: {
+            type: Schema.Types.Boolean,
+            default: false,
+          },
+          timestamp: {
+            type: Schema.Types.Date,
+          },
+        },
+      ],
     },
   ],
 })
