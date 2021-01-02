500f35e1a9fadd30153f8025b0d80754488b72bf
medjamiaanes <medjamia.anes@gmail.com>
2021-01-02 21:26:14 +0100

    Enhanced the inbox fetch method & added the AddChat endpoint

--- src/api/controllers/InboxController.js
+++ src/api/controllers/InboxController.js
@@ -2,13 +2,42 @@ const { Inbox } = require('../models')
 const { LoggerService } = require('../../services')
 
 exports.fetch = async (req, res) => {
-  const { userId } = req.query
-
   try {
-    const inbox = await Inbox.find({ userId }).populate(
-      'userId messages',
-    )
+    console.log(req.user)
+    const inbox = await Inbox.findOne({
+      user: req.user._id,
+    }).populate('chats.user')
     return res.status(200).json(inbox)
+  } catch (error) {
+    LoggerService.serverError(error)
+    return res.status(500).json({ message: 'Server Error' })
+  }
+}
+
+exports.addChat = async (req, res) => {
+  const { userId } = req.body
+  let inbox
+  try {
+    inbox = await Inbox.findOne({ user: req.user._id })
+    if (inbox) {
+      await Inbox.updateOne(
+        { _id: inbox._id },
+        { $set: { chats: [{ user: userId, messages: [] }] } },
+      )
+      inbox = await Inbox.findOne({
+        user: req.user._id,
+      }).populate('chats.user')
+      return res.status(200).json(inbox)
+    }
+    inbox = new Inbox({
+      user: req.user._id,
+      chats: [{ user: userId, messages: [] }],
+    })
+    await inbox.save()
+    inbox = await Inbox.findOne({
+      user: req.user._id,
+    }).populate('chats.user')
+    return res.status(201).json(inbox)
   } catch (error) {
     LoggerService.serverError(error)
     return res.status(500).json({ message: 'Server Error' })
--- src/api/routes/InboxRouter.js
+++ src/api/routes/InboxRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const { InboxController } = require('../controllers')
 
 router.get('/fetch', InboxController.fetch)
+router.post('/add/chat', InboxController.addChat)
 
 module.exports = router
