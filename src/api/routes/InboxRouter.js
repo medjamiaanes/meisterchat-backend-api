d388cae6ca330f3719b0e5674bc86ff86873388d
medjamiaanes <medjamia.anes@gmail.com>
2021-01-30 14:22:27 +0100

    Added remove chat route

--- src/api/routes/InboxRouter.js
+++ src/api/routes/InboxRouter.js
@@ -3,5 +3,6 @@ const { InboxController } = require('../controllers')
 
 router.get('/fetch', InboxController.fetch)
 router.post('/add/chat', InboxController.addChat)
+router.delete('/remove/chat', InboxController.removeChat)
 
 module.exports = router
